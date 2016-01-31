f7699941de36f73071a669a08657e9069852fcfc
tomoya <ihatov08@gmail.com>
2016-02-01 00:52:09 +0900

    controllerの修正

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -1,28 +1,28 @@
 class InquiryController < ApplicationController
-  def index
-    #入力画面を表示
+def index
+    # 入力画面を表示
     @inquiry = Inquiry.new
     render :action => 'index'
   end
 
   def confirm
-    #入力値のチェック
+    # 入力値のチェック
     @inquiry = Inquiry.new(params[:inquiry])
     if @inquiry.valid?
-      #OK 確認画面を表示
+      # OK。確認画面を表示
       render :action => 'confirm'
     else
-      #NG 入力画面を再表示
+      # NG。入力画面を再表示
       render :action => 'index'
     end
   end
 
   def thanks
-    #メール送信
+    # メール送信
     @inquiry = Inquiry.new(params[:inquiry])
     InquiryMailer.received_email(@inquiry).deliver
 
-    #完了画面を表示
+    # 完了画面を表示
     render :action => 'thanks'
   end
 end
